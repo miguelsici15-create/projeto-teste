e901db8794aa55570b4578f3ef4991dcda7e81b4
Miguel pereira <miguelsici15@gmail.com>
2026-08-10 13:14:32 -0300

    bu

---
+++ src/app/features/detalhes/detalhes.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-detalhes',
+  imports: [],
+  templateUrl: './detalhes.html',
+  styleUrl: './detalhes.css',
+})
+export class Detalhes {}
