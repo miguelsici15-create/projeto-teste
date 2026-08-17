70cd032a848421eb47b5521fbf7eac75f970532c
Miguel pereira <miguelsici15@gmail.com>
2026-08-17 15:43:19 -0300

    top

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,15 @@
 import { Routes } from '@angular/router';
 import { ListaProdutos } from './features/produtos/lista-produtos/lista-produtos';
 import { Carrinho } from './features/carrinho/carrinho/carrinho';
+import { Home } from './features/home/home/home';
 
 export const routes: Routes = [
     {path: '',
+        component: Home
+    },
+    {path:'produtos',
         component: ListaProdutos
+
     },
     {
         path: 'carrinho',
